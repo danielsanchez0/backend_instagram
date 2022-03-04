4d957acf16015d2676ac206ef4f191597ca864f7
Daniel Sanchez <daniel991938@gmail.com>
2022-03-04 15:18:55 -0500

    agregada documentacion de la ruta user

--- routes/user.js
+++ routes/user.js
@@ -10,9 +10,15 @@ const User = mongoose.model("User");
  * /user/stats:
  *   get:
  *     description: Retorna la lista de usuarios con la información correspondiente a su id,nombre,url de su foto de perfil y cantidad de posts realizados, en orden respecto a la cantidad de posts.
+ *     produces:
+ *       - application/json
  *     responses:
  *       200:
  *         description: Retorna un json con la información solicitada correctamente.
+ *         type: json
+ *       401:
+ *         description: Error de acceso, debe estar logueado para poder acceder a esta información.
+ *         type: json
  */
 router.get('/stats',requireLogin,(req,res)=>{
 	User.aggregate([{ "$lookup": {
@@ -33,11 +39,35 @@ router.get('/stats',requireLogin,(req,res)=>{
 	.then(posts=>{
 		res.status(200).json({posts})
 	}).catch(err=>{
-		res.status(404).json({});
 		console.log(err);
 	})
 })
 
+/**
+ * @swagger
+ * /user/user/{id}:
+ *   get:
+ *     description: Usada para solicitar la información de usuario a traves de su ID de identificación.
+ *     produces:
+ *       - application/json
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         description: ID de un usuario
+ *     responses:
+ *       200:
+ *         description: Retorna la información de usuario correspondiente al ID proporcionado.
+ *         type: json
+ *       401:
+ *         description: Error de acceso, debe estar logueado para poder acceder a esta información.
+ *         type: json
+ *       404:
+ *         description: No existe usuario correspondiente al ID proporcionado.
+ *         type: json
+ *       422:
+ *         description: Error de validación de los datos.
+ *         type: json
+ */
 router.get('/user/:id',requireLogin,(req,res)=>{
 	User.findOne({_id:req.params.id})
 	.select("-password").
@@ -49,13 +79,32 @@ router.get('/user/:id',requireLogin,(req,res)=>{
 				return res.status(422).json({error:err})
 			}
 
-			res.json({user,posts})
+			res.status(200).json({user,posts})
 		})
 	}).catch(err=>{
 		return res.status(404).json({error:"user not found"})
 	})
 })
 
+/**
+ * @swagger
+ * /user/follow:
+ *   put:
+ *     description: Usada para solicitar agregar un nuevo usuario a la lista de usuarios seguidos.
+ *     produces:
+ *       - application/json
+ *     parameters:
+ *       - in: body
+ *         name: followId
+ *         description: ID del usuario que deseamos seguir.
+ *     responses:
+ *       401:
+ *         description: Error de acceso, debe estar logueado para poder acceder a esta información.
+ *         type: json
+ *       422:
+ *         description: Error de validación de los datos.
+ *         type: json
+ */
 router.put('/follow',requireLogin,(req,res)=>{
 	User.findByIdAndUpdate(req.body.followId,{
 		$push:{followers:req.user._id}
@@ -77,6 +126,25 @@ router.put('/follow',requireLogin,(req,res)=>{
 	})
 })
 
+/**
+ * @swagger
+ * /user/unfollow:
+ *   put:
+ *     description: Usada para solicitar quitar un usuario de la lista de usuarios seguidos.
+ *     produces:
+ *       - application/json
+ *     parameters:
+ *       - in: body
+ *         name: unfollowId
+ *         description: ID del usuario que deseamos dejar de seguir.
+ *     responses:
+ *       401:
+ *         description: Error de acceso, debe estar logueado para poder acceder a esta información.
+ *         type: json
+ *       422:
+ *         description: Error de validación de los datos.
+ *         type: json
+ */
 router.put('/unfollow',requireLogin,(req,res)=>{
 	User.findByIdAndUpdate(req.body.unfollowId,{
 		$pull:{followers:req.user._id}
@@ -98,6 +166,25 @@ router.put('/unfollow',requireLogin,(req,res)=>{
 	})
 })
 
+/**
+ * @swagger
+ * /user/updatepic:
+ *   put:
+ *     description: Usada para actializar el enlace de la foto de perfil.
+ *     produces:
+ *       - application/json
+ *     parameters:
+ *       - in: body
+ *         name: pic
+ *         description: Enlace de la foto de perfil.
+ *     responses:
+ *       401:
+ *         description: Error de acceso, debe estar logueado para poder acceder a esta información.
+ *         type: json
+ *       422:
+ *         description: Error de validación de los datos.
+ *         type: json
+ */
 router.put('/updatepic',requireLogin,(req,res)=>{
 	User.findByIdAndUpdate(req.user._id,{$set:{pic:req.body.pic}},
 		{new:true},
